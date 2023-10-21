c0028d5377140cd594ff9e0fa7fea0918cb28fbb
Phornhathai <phornhathai.aum@gmail.com>
2023-10-21 17:00:07 +0700

    complete Day 19

---
+++ Day 19 React 101/src/Mybutton.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function Mybutton() {
+    return (
+        <button type="submit">Submit</button>
+    )
+}
+
+export default Mybutton
--- Day 19 React 101/src/main.tsx
+++ Day 19 React 101/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Mybutton from './Mybutton.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <App/>
+    <Mybutton/>
   </React.StrictMode>,
 )
