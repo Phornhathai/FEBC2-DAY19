64b8be6fd209c309bb2e85060d13b265917ed1ed
Phornhathai <phornhathai.aum@gmail.com>
2023-10-27 17:45:12 +0700

    add List.tsx

--- Day 19 React 101/src/App.tsx
+++ Day 19 React 101/src/App.tsx
@@ -1,6 +1,7 @@
 import Heading from "./components/heading.tsx"
 import Section from "./components/Section.tsx"
 import Counter from "./components/Counter.tsx"
+import List from "./components/List.tsx"
 import { useState } from "react";
 
 
@@ -8,9 +9,15 @@ function App() {
   //ประกาศตัวแปร และค่าเริ่มต้นที่ใช้ useState เก็บค่าประเภท number return กลับไป
   const [count, setCount] = useState<number>(1);
   return (<>
+    <List items ={["🌟 Star "," 🍴 fork ","🔥 Fire"]} render={(item) => <b>{item}</b>}></List>
     <Heading title="Hello it's me"></Heading>
-    <Section title="Aum">Hello world</Section>
+    {/* title = prop , Hello world = child node */}
+    <Section title="Aum">
+      Hello world
+      Bye Bye world
+    </Section>
     <Counter setCount={setCount}>Counter = {count}</Counter>
+    
   </>);
 }
 export default App
--- Day 19 React 101/src/components/Counter.tsx
+++ Day 19 React 101/src/components/Counter.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react"
 
 type countProps = {
+    // เป็นการโยนข้อมูล setCount มาเป็น number
     setCount : React.Dispatch<React.SetStateAction<number>>
     children : React.ReactNode;
 }
@@ -10,10 +11,14 @@ const Counter = ({setCount,children }: countProps) => {
     return (
         <>
             <div>
-                <p>{children}</p>
+                {/* <p>{children}</p> */}
+                <h1>{children}</h1>
             </div>
-            <button onClick={() => setCount(prev => prev + 1)}>Increment</button>
-            <button onClick={() => setCount(prev => prev - 1)}>Decrement</button>
+            {/* <button onClick={() => setCount(prev => prev + 1)}>Increment</button>
+            <button onClick={() => setCount(prev => prev - 1)}>Decrement</button> */}
+            <button onMouseOut={() => setCount(prev => prev + 1)}>Increment</button>
+            <button onMouseOut={() => setCount(prev => prev - 1)}>Decrement</button>
+            
         </>
     )
 }
