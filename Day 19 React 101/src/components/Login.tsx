75b537cb76c9b6fb91ec9ff2941594c0f2594a8c
Phornhathai <phornhathai.aum@gmail.com>
2023-10-29 16:28:34 +0700

    add form submit and event changeUsername changePassword and onSubmit

--- Day 19 React 101/src/components/Login.tsx
+++ Day 19 React 101/src/components/Login.tsx
@@ -1,13 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const Login = () => {
-    // if (isLogin) {
-    //     return (<div>Dashboard</div>)
+    const [Username, setUsername] = useState<string>();
+    const [Password, setPassword] = useState<string>();
 
-    // } else {
-    //     return (<div>Not Login</div>)
-    // }
-    return <h1>Login</h1>
+    const onChangeUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value)
+        console.log(Username)
+    }
+    const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value)
+        console.log(Password)
+    }
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        console.log("Submitted")
+    }
+    return (
+        <>
+        <div>
+        <form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id='username' name='name' onChange={onChangeUsername} />
+        <input type="password" id='password' name='name' onChange={onChangePassword}/>
+        </form>
+        </div>
+        </>
+    )
 }
+
+
 
 export default Login
